0757d50cc1ef26bd340d3d28456f0cce117ea8f2
James Schumann <jschumann@gmail.com>
2019-07-30 08:42:46 -0400

    DRY up cats and tags display at bottom of content partials

--- partials/content.php
+++ partials/content.php
@@ -49,39 +49,4 @@ if (has_excerpt()) {
     the_excerpt();
 }
 
-$igc=0;
-foreach ((get_the_category()) as $category) {
-    if (strtolower($category->cat_name) != 'uncategorized') {
-        $igc = 1;
-    }
-}
-
-if ($igc == 1) {
-    $display_cats = 1;
-}
-
-$number_of_tags = count(get_the_tags());
-
-//if ($number_of_tags > 0) {
-if (get_the_tags()) {
-    $display_tags = 1;
-}
-
-if (!isset($display_cats) && isset($display_tags)) {
-    echo '<div class="clearfix"></div>';
-    echo '<p class="categories" role="menubar">Tags: ';
-    the_tags('', ' &middot; ', '');
-    echo '</p>';
-} elseif (isset($display_cats) && !isset($display_tags)) {
-    echo '<div class="clearfix"></div>';
-    echo '<p class="categories" role="menubar">Categories: ';
-    the_category(' &middot; ');
-    echo '</p>';
-} elseif (isset($display_cats) && isset($display_tags)) {
-    echo '<div class="clearfix"></div>';
-    echo '<p class="categories" role="menubar">Categories: ';
-    the_category(' &middot; ');
-    echo ' Tags: ';
-    the_tags('', ' &middot; ', '');
-    echo '</p>';
-}
+get_template_part('partials/content', 'cats-tags');
