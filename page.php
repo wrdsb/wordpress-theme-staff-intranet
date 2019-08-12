8a4b54c29ac96813148c5e7e25f896a64a174b0d
James Schumann <jschumann@gmail.com>
2019-08-12 07:43:42 -0400

    linting

--- page.php
+++ page.php
@@ -31,7 +31,7 @@
 
             # Both sidebars
             # left column
-            if (($has_left === true) and ($has_right === true)):
+            if (($has_left === true) and ($has_right === true)) {
                 echo '<div class="col-sm-3 col-md-2 col-lg-2" role="complementary">';
                 if (is_front_page()) {
                     get_template_part('partials/sidebar', 'left');
@@ -42,7 +42,7 @@
                 echo '</div>';
 
             # Just left sidebar
-            elseif (($has_left === true) and ($has_right === false)):
+            } elseif (($has_left === true) and ($has_right === false)) {
                 echo '<div class="col-sm-3 col-lg-3" role="complementary">';
                 if (is_front_page()) {
                     get_template_part('partials/sidebar', 'left');
@@ -51,59 +51,65 @@
                     get_template_part('partials/sidebar', 'left');
                 }
                 echo '</div>';
+
             # Just right sidebar
-            elseif (($has_left === false) and ($has_right === true)):
-            # Nothing to do
+            } elseif (($has_left === false) and ($has_right === true)) {
+                # Nothing to do
+    
             # No sidebars
-            elseif (($has_left === false) and ($has_right === false)):
-            # Nothing to do
-            endif; ?>
+            } elseif (($has_left === false) and ($has_right === false)) {
+                # Nothing to do
+            } ?>
 
             <?php
             # Both sidebars
             # content area
-            if (($has_left === true) and ($has_right === true)):
+            if (($has_left === true) and ($has_right === true)) {
                 echo '<div class="col-sm-6 col-md-8 col-lg-8" role="main">';
-            # Just left sidebar
-            elseif (($has_left === true) and ($has_right === false)):
+                # Just left sidebar
+            } elseif (($has_left === true) and ($has_right === false)) {
                 echo '<div class="col-sm-9 col-lg-9" role="main">';
             # Just right sidebar
-            elseif (($has_left === false) and ($has_right === true)):
+            } elseif (($has_left === false) and ($has_right === true)) {
                 echo '<div class="col-sm-9" role="main">';
             # No sidebars
-            elseif (($has_left === false) and ($has_right === false)):
+            } elseif (($has_left === false) and ($has_right === false)) {
                 echo '<div class="col-sm-12 col-lg-12" role="main">';
-            endif; ?>
+            } ?>
 
             <?php // check if the post has a Post Thumbnail assigned to it.
             if (has_post_thumbnail()) {
                 echo '<div class="featuredimage" rol="presentation">';
-                if (($has_left === true) and ($has_right === true)):
-                    the_post_thumbnail('wrdsb-two-sidebars', 'alt'); elseif (($has_left === true) and ($has_right === false)):
-                    the_post_thumbnail('wrdsb-one-sidebar', 'alt'); elseif (($has_left === false) and ($has_right === true)):
-                    the_post_thumbnail('wrdsb-one-sidebar', 'alt'); elseif (($has_left === false) and ($has_right === false)):
+                if (($has_left === true) and ($has_right === true)) {
+                    the_post_thumbnail('wrdsb-two-sidebars', 'alt');
+                } elseif (($has_left === true) and ($has_right === false)) {
+                    the_post_thumbnail('wrdsb-one-sidebar', 'alt');
+                } elseif (($has_left === false) and ($has_right === true)) {
+                    the_post_thumbnail('wrdsb-one-sidebar', 'alt');
+                } elseif (($has_left === false) and ($has_right === false)) {
                     the_post_thumbnail('wrdsb-full-width', 'alt');
-                endif;
+                }
                 echo '</div>';
             } ?>
 
             <?php
             // Start the Loop.
-            while (have_posts()) : the_post();
+            while (have_posts()) {
+                the_post();
                 // Include the page content template.
                 get_template_part('partials/content', 'page');
                 // If comments are open or we have at least one comment, load up the comment template.
                 if (comments_open() || get_comments_number()) {
                     comments_template();
                 }
-            endwhile; ?>
+            } ?>
 
             </div> <!-- end content area -->
 
             <?php
             # Both sidebars
             # right column
-            if (($has_left === true) and ($has_right === true)):
+            if (($has_left === true) and ($has_right === true)) {
                 echo '<div class="col-sm-3 col-md-2 col-lg-2" role="complementary">';
                 if (is_front_page()) {
                     get_template_part('partials/sidebar', 'right');
@@ -115,7 +121,7 @@
                 # Just left sidebar
                 # Nothing to do
                 # Just right sidebar
-            elseif (($has_left === false) and ($has_right === true)):
+            } elseif (($has_left === false) and ($has_right === true)) {
                 echo '<div class="col-sm-3" role="complementary">';
                 if (is_front_page()) {
                     get_template_part('partials/sidebar', 'right');
@@ -126,8 +132,7 @@
                 echo '</div>';
                 # No sidebars
                 # Nothing to do
-            endif;
-            ?>
+            }; ?>
         </div>
     </div>
 <?php } ?>
